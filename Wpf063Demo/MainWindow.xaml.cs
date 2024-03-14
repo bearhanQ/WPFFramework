2130d1b84189aea675e13b6e5d10b72af3cf419d
han.qin <976802198@qq.com>
2024-03-14 18:19:31 +0800

    add dragmove function

--- Wpf063Demo/MainWindow.xaml.cs
+++ Wpf063Demo/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace Wpf063Demo
         {
             InitializeComponent();
         }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
+        }
     }
 }
