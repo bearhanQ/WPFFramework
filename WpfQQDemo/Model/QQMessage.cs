7f6e0eb2d118b720e2bcf4f2c53144650c4b1719
qinhan hanshuai <976802198@qq.com>
2024-05-12 22:19:04 +0800

    QQMessage model

---
+++ WpfQQDemo/Model/QQMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfQQDemo.Model
+{
+    public class QQMessage
+    {
+        public string UserName { get; set; }
+        public string Profile { get; set; }
+        public string Time { get; set; }
+        public string Message { get; set; }
+    }
+}
