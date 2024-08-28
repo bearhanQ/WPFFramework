2fba3f85962559bf0aa7de38486885cbfff651b5
han.qin <976802198@qq.com>
2024-08-28 15:10:08 +0800

    add a wave effect in normal button

--- WPFTemplate/Controls/CornerButton.cs
+++ WPFTemplate/Controls/CornerButton.cs
@@ -31,6 +31,8 @@ namespace WPFTemplate
 
         public static readonly DependencyProperty PressShowShadowProperty;
 
+        private Border _shadowBorder;
+
         public CornerRadius CornerRadius
         {
             get { return (CornerRadius)GetValue(CornerRadiusProperty); }
@@ -71,6 +73,95 @@ namespace WPFTemplate
             MouseOverForegroundProperty = DependencyProperty.Register("MouseOverForeground", typeof(Brush), typeof(CornerButton), new PropertyMetadata(Brushes.Black));
             LinkShowUnderLineProperty = DependencyProperty.Register("LinkShowUnderLine", typeof(bool), typeof(CornerButton), new PropertyMetadata(true));
             PressShowShadowProperty = DependencyProperty.Register("PressShowShadow", typeof(bool), typeof(CornerButton), new PropertyMetadata(true));
+
+            EventManager.RegisterClassHandler(typeof(CornerButton), UIElement.MouseEnterEvent, new RoutedEventHandler(OnMouseEnterEvent));
+            EventManager.RegisterClassHandler(typeof(CornerButton), UIElement.MouseLeaveEvent, new RoutedEventHandler(OnMouseLeaveEvent));
+        }
+
+        public static void OnMouseEnterEvent(object sender, RoutedEventArgs e)
+        {
+            var button = sender as CornerButton;
+            if (button != null && button.ButtonType == ButtonType.Normal)
+            {
+                var shadowBoder = button._shadowBorder;
+                if (shadowBoder != null)
+                {
+                    Storyboard storyboard = new Storyboard();
+                    RectAnimationUsingKeyFrames animation = new RectAnimationUsingKeyFrames
+                    {
+                        KeyFrames = new RectKeyFrameCollection
+                        {
+                            new EasingRectKeyFrame
+                            {
+                                KeyTime = TimeSpan.FromSeconds(0),
+                                Value = new Rect
+                                {
+                                    X = button.ActualWidth / 2,
+                                    Y = 0,
+                                    Width = 0,
+                                    Height = button.ActualHeight
+                                }
+                            },
+                            new EasingRectKeyFrame
+                            {
+                                KeyTime = TimeSpan.FromMilliseconds(300),
+                                Value = new Rect
+                                {
+                                    X = 0,
+                                    Y = 0,
+                                    Width = button.ActualWidth,
+                                    Height = button.ActualHeight
+                                }
+                            }
+                        }
+                    };
+                    Storyboard.SetTarget(animation, button._shadowBorder);
+                    Storyboard.SetTargetProperty(animation, new PropertyPath("(UIElement.Clip).(RectangleGeometry.Rect)"));
+                    storyboard.Children.Add(animation);
+                    storyboard.Begin();
+                }
+            }
+        }
+
+        public static void OnMouseLeaveEvent(object sender, RoutedEventArgs e)
+        {
+            var button = sender as CornerButton;
+            if (button != null && button.ButtonType == ButtonType.Normal)
+            {
+                button.CreateClip();
+            }
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _shadowBorder = this.GetTemplateChild("PART_ShadowBorder") as Border;
+            CreateClip();
+        }
+
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            base.OnRenderSizeChanged(sizeInfo);
+            CreateClip();
+        }
+
+        private void CreateClip()
+        {
+            if (_shadowBorder != null)
+            {
+                var X = this.ActualWidth;
+                var Height = this.ActualHeight;
+                _shadowBorder.Clip = new RectangleGeometry
+                {
+                    Rect = new Rect
+                    {
+                        X = X / 2,
+                        Y = 0,
+                        Width = 0,
+                        Height = Height
+                    }
+                };
+            }
         }
     }
 }
