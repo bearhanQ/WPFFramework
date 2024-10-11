d7ec1215961f2850de0d3533114b57deb9d7fb48
han.qin <976802198@qq.com>
2024-10-11 13:32:17 +0800

    add a cornerradius property to datepicker

--- WPFTemplate/Controls/Original/CornerDatePicker.cs
+++ WPFTemplate/Controls/Original/CornerDatePicker.cs
@@ -17,9 +17,18 @@ namespace WPFTemplate
 {
     public class CornerDatePicker : DatePicker
     {
+        public static readonly DependencyProperty CornerRadiusProperty;
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
         static CornerDatePicker()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CornerDatePicker), new FrameworkPropertyMetadata(typeof(CornerDatePicker)));
+            CornerRadiusProperty = DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(CornerDatePicker));
         }
     }
 }
